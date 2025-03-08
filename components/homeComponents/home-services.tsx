f5e00c34fe69fd25e8154cf60ea19136495d8255
Abdul Rehman <arehman8125@gmail.com>
2025-03-08 13:03:16 +0500

    add services

--- components/homeComponents/home-services.tsx
+++ components/homeComponents/home-services.tsx
@@ -1,24 +1,29 @@
 "use client";
 import { FaWalking, FaTruck } from "react-icons/fa";
 import TextBox from "../text-box";
+import Image from "next/image";
 
 export default function HomeServices() {
   return (
-    <section className="py-12 bg-gray-100 container mx-auto">
+    <section className="py-12 mx-auto">
       <TextBox text="Services" />
-      <div className="container mx-auto px-6 lg:px-20 flex flex-col lg:flex-row items-center gap-8">
+      <h2 className="text-2xl font-bold px-2 sm:px-4 md:px-8 lg:px-12 py-2">
+        We Offer You the Best<span className="text-red-500">,</span>
+      </h2>
+      <div className="mx-auto px-2 sm:px-4 md:px-6 lg:px-20 flex flex-col lg:flex-row items-center gap-4 md:gap-8 py-2 md:py-0">
         {/* Left Side: Features */}
-        <div className="flex flex-col gap-6 lg:w-1/2">
-          <h2 className="text-2xl font-bold">
-            We Offer You the Best<span className="text-red-500">,</span>
-          </h2>
-
-          <div className="flex gap-6">
+        <div className="flex flex-col gap-3 sm:gap-5 md:gap-6 lg:w-1/2">
+          <div className="flex gap-3 sm:gap-5 md:gap-6">
             {/* Personal Pick Up */}
-            <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-start border-t-4 border-red-400">
-              <div className="flex items-center justify-between gap-2">
-                <FaWalking className="text-red-500 text-2xl" />
-                <span className="text-gray-300 text-2xl font-extrabold">
+            <div className="bg-white shadow-lg rounded-xl p-6 flex flex-col items-start">
+              <div className="flex flex-row items-center justify-between gap-2 w-full">
+                <Image
+                  src={"/person.svg"}
+                  width={50} // Increased for iPad
+                  height={50}
+                  alt={"person"}
+                />
+                <span className="text-gray-300 text-5xl font-extrabold">
                   01
                 </span>
               </div>
@@ -30,10 +35,15 @@ export default function HomeServices() {
             </div>
 
             {/* Free Delivery */}
-            <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-start border-t-4 border-red-400">
-              <div className="flex items-center gap-2">
-                <FaTruck className="text-red-500 text-2xl" />
-                <span className="text-gray-300 text-2xl font-extrabold">
+            <div className="bg-white shadow-lg rounded-xl p-6 flex flex-col items-start">
+              <div className="flex flex-row items-center justify-between gap-2 w-full">
+                <Image
+                  src={"/bus.svg"}
+                  width={50} // Increased for iPad
+                  height={50}
+                  alt={"bus"}
+                />
+                <span className="text-gray-300 text-5xl font-extrabold">
                   02
                 </span>
               </div>
@@ -48,10 +58,12 @@ export default function HomeServices() {
 
         {/* Right Side: Image */}
         <div className="lg:w-1/2">
-          <img
-            src="/delivery-route.png"
-            alt="Delivery Route"
-            className="w-full h-auto"
+          <Image
+            src={"/route.svg"}
+            width={100} // Increased for iPad
+            height={100}
+            alt={"route"}
+            className="w-full shadow-xl"
           />
         </div>
       </div>
