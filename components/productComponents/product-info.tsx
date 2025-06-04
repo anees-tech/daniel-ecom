90845bd0f20f52e30d7710a012889f5318fdd434
Abdul Rehman <arehman8125@gmail.com>
2025-06-04 20:03:14 +0500

    product detail

--- components/productComponents/product-info.tsx
+++ components/productComponents/product-info.tsx
@@ -1,5 +1,6 @@
 "use client";
 // 
+// 
 import { Star, Plus, Minus } from "lucide-react";
 import { useCartStore } from "@/context/addToCartContext";
 import ProductReviewModal from "./product-reiw-modal";
